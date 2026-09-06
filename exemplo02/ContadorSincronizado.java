7b04373a2845aec2fddea8683a848b824f85df5a
Wesley Dias Maciel <wdiasmaciel@gmail.com>
2026-09-06 21:10:53 +0000

    Exemplo

---
+++ exemplo02/ContadorSincronizado.java
@@ -0,0 +1,26 @@
+public class ContadorSincronizado {
+    private int contador = 0;
+
+    /*
+     * A palavra-chave 'synchronized' transforma um bloco em
+     * uma Região Crítica. Se a Thread A entrar neste bloco, a 
+     * Thread B será bloqueada na entrada automaticamente.
+     */
+    public void incrementar(String nome) {
+
+        synchronized (this) {
+            System.out.println(nome + " entrou no bloco sincronizado.");
+            System.out.flush();
+            contador++;
+            System.out.println(nome + " alterou o valor para: " + contador + ".");
+            System.out.flush();
+        }
+
+        System.out.println(nome + " saiu do bloco sincronizado.");
+        System.out.flush();
+    }
+
+    public int getContador() {
+        return contador;
+    }
+}
